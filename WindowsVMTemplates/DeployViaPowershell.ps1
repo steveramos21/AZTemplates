d3d00a9fcf770eb5fd2e676fdbfe9726267871ed
steveramos21 <steveramos21@gmail.com>
2021-06-06 19:26:39 +0800

    6.6.21 - Adding ReadMe description6.6.21 - Adding remove vm template via powershell

--- WindowsVMTemplates/DeployViaPowershell.ps1
+++ WindowsVMTemplates/DeployViaPowershell.ps1
@@ -11,3 +11,6 @@ $setSubscription
 New-AzResourceGroupDeployment -Name VMtepmlate -ResourceGroupName TempResources `
 -TemplateFile ..\GitAZTemplate\WindowsVMTemplates\FirstVMTemplate.json -verbose
 
+#REMOVE Resources 
+New-AzResourceGroupDeployment -Name RemoveTemp -ResourceGroupName TempResources `
+-TemplateFile ..\GitAZTemplate\WindowsVMTemplates\RemoveVMTemplate.json -Mode Complete -verbose 
