56026c4cda792ada88b817c899a457d02d147963
steveramos21 <steveramos21@gmail.com>
2021-06-06 12:39:50 +0800

    6.6.21 - Adding powershell deployment script.

---
+++ WindowsVMTemplates/DeployViaPowershell.ps1
@@ -0,0 +1,12 @@
+#Connect to AZ account
+Connect-AzAccount
+
+#Check Subscriptions
+$subscription = Get-AzSubscription
+
+#SELECT subscriptions 
+$setSubscription = Select-AzSubscription -Tenant $subscription.TenantId.GetValue(1)
+$setSubscription
+
+New-AzResourceGroupDeployment -Name VMtepmlate -ResourceGroupName TempResources `
+-TemplateFile ..\GitAZTemplate\WindowsVMTemplates\FirstVMTemplate.json -verbose
